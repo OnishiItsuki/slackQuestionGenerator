86e40f5f3188135c8fea86e460b15416b8358638
OnishiItsuki <tsukiouyc4663aobs@gmail.com>
2023-02-26 22:36:07 +0900

    ディレクトリ名をtypo

--- src/usecase/slackQuestionController/index.ts
+++ src/usecase/slackQuestionController/index.ts
@@ -151,3 +151,145 @@ export default class SlackQuestionController {
     return ContentService.createTextOutput();
   }
 }
+
+const main = () => {
+  const o = new SlackQuestionController({
+    payload: {},
+    slackToken: "",
+    slackPostUrl: "",
+  });
+
+  const IS_DEBUGGING = false;
+  o.setQuestionBlocks(
+    [
+    {
+      blockType: "static_select",
+      params: {
+        block_id: "service",
+        label: "サービス",
+        optional: IS_DEBUGGING,
+        options: [
+          {
+            label: "aircloset",
+            value: "aircloset",
+          },
+          {
+            label: "fitting",
+            value: "fitting",
+          },
+          {
+            label: "bridge",
+            value: "bridge",
+          },
+          {
+            label: "mall",
+            value: "mall",
+          },
+        ],
+      },
+    },
+
+    {
+      blockType: "static_select",
+      params: {
+        block_id: "operation_type",
+        label: "依頼項目",
+        optional: IS_DEBUGGING,
+        options: [
+          {
+            label: "調査依頼",
+            value: "search",
+          },
+          {
+            label: "データ修正依頼",
+            value: "data_modify",
+          },
+          {
+            label: "SQLレビュー依頼",
+            value: "sql_review",
+          },
+        ],
+      },
+    },
+
+    {
+      blockType: "plain_text_input",
+      params: {
+        block_id: "request_background",
+        label: "背景",
+        optional: IS_DEBUGGING,
+        multiline: true,
+      },
+    },
+
+    {
+      blockType: "plain_text_input",
+      params: {
+        block_id: "request_detail",
+        optional: IS_DEBUGGING,
+        multiline: true,
+        label: "依頼内容",
+      },
+    },
+
+    {
+      blockType: "number_input",
+      params: {
+        block_id: "user_id",
+        label: "ユーザーID",
+        optional: IS_DEBUGGING,
+        is_decimal_allowed: true,
+        min_value: "0",
+      },
+    },
+
+    {
+      blockType: "number_input",
+      params: {
+        block_id: "order_id",
+        label: "オーダーID",
+        optional: true,
+        is_decimal_allowed: true,
+        min_value: "0",
+      },
+    },
+
+    {
+      blockType: "static_select",
+      params: {
+        block_id: "severity_type",
+        label: "緊急度",
+        optional: IS_DEBUGGING,
+        options: [
+          {
+            label: "当日必須",
+            value: "in_today",
+          },
+          {
+            label: "3日以内",
+            value: "in_three_days",
+          },
+          {
+            label: "7日以内",
+            value: "in_seven_days",
+          },
+          {
+            label: "1週間以上",
+            value: "over_one_week",
+          },
+        ],
+      },
+    },
+
+    {
+      blockType: "datepicker",
+      params: {
+        label: "対応期限",
+        block_id: "limit_date",
+        optional: IS_DEBUGGING,
+      },
+    },
+  ]
+  
+  );
+};

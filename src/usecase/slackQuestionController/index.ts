7acd04bda5777c21236e0660f8634c29fbeb8cd2
OnishiItsuki <tsukiouyc4663aobs@gmail.com>
2023-03-06 17:13:03 +0900

    🚧 GAS上で実行できるようにしたがでバックできていない

--- src/usecase/slackQuestionController/index.ts
+++ src/usecase/slackQuestionController/index.ts
@@ -11,13 +11,12 @@ type Payload = Record<string, any>;
 export default class SlackQuestionController {
   private _isOpenModal: boolean = false;
   public OPEN_MODAL_URL: string = "https://slack.com/api/views.open";
+  private _SLACK_TOKEN: string;
+  private _TRIGGER_ID: string;
 
   private _questionTitle: string = "";
   private _questionBlockConfigList: GenerateBlockConfig<BlockTypes>[] = [];
-
-  private _slackPostService: SlackPostQuestionService;
   private _slackQuestionBlockGenerator: SlackQuestionBlockGenerator;
-
   private _result: null | Record<string, string> = null;
 
   private _throwError(message: string): never {
@@ -64,20 +63,15 @@ export default class SlackQuestionController {
     this._result = null;
   }
 
-  constructor({
-    payload,
-    slackToken,
-    slackPostUrl,
-  }: {
-    payload: any;
-    slackToken: string;
-    slackPostUrl: string;
-  }) {
-    this._slackPostService = new SlackPostQuestionService({
-      token: slackToken,
-      triggerId: payload?.trigger_id,
-      postUrl: slackPostUrl,
-    });
+  constructor({ payload, slackToken }: { payload: any; slackToken: string }) {
+    if (!payload?.trigger_id) {
+      this._throwError(
+        `payload.trigger_id is not defined: ${JSON.stringify(payload)}`
+      );
+    }
+
+    this._TRIGGER_ID = payload.trigger_id;
+    this._SLACK_TOKEN = slackToken;
     this._slackQuestionBlockGenerator = new SlackQuestionBlockGenerator(
       this._createActionId
     );
@@ -93,12 +87,12 @@ export default class SlackQuestionController {
     }
   }
 
-  set title(title: string) {
+  set questionTitle(title: string) {
     this._checkOpenModal();
     this._questionTitle = title;
   }
 
-  get title(): string {
+  get questionTitle(): string {
     return this._questionTitle;
   }
 
@@ -145,151 +139,15 @@ export default class SlackQuestionController {
       this._questionBlockConfigList
     );
 
-    this._slackPostService.post(params);
+    const slackPostService = new SlackPostQuestionService({
+      token: this._SLACK_TOKEN,
+      triggerId: this._TRIGGER_ID,
+      postUrl: this.OPEN_MODAL_URL,
+    });
+    slackPostService.post(params);
 
     this._isOpenModal = true;
+
     return ContentService.createTextOutput();
   }
 }
-
-const main = () => {
-  const o = new SlackQuestionController({
-    payload: {},
-    slackToken: "",
-    slackPostUrl: "",
-  });
-
-  const IS_DEBUGGING = false;
-  o.setQuestionBlocks(
-    [
-    {
-      blockType: "static_select",
-      params: {
-        block_id: "service",
-        label: "サービス",
-        optional: IS_DEBUGGING,
-        options: [
-          {
-            label: "aircloset",
-            value: "aircloset",
-          },
-          {
-            label: "fitting",
-            value: "fitting",
-          },
-          {
-            label: "bridge",
-            value: "bridge",
-          },
-          {
-            label: "mall",
-            value: "mall",
-          },
-        ],
-      },
-    },
-
-    {
-      blockType: "static_select",
-      params: {
-        block_id: "operation_type",
-        label: "依頼項目",
-        optional: IS_DEBUGGING,
-        options: [
-          {
-            label: "調査依頼",
-            value: "search",
-          },
-          {
-            label: "データ修正依頼",
-            value: "data_modify",
-          },
-          {
-            label: "SQLレビュー依頼",
-            value: "sql_review",
-          },
-        ],
-      },
-    },
-
-    {
-      blockType: "plain_text_input",
-      params: {
-        block_id: "request_background",
-        label: "背景",
-        optional: IS_DEBUGGING,
-        multiline: true,
-      },
-    },
-
-    {
-      blockType: "plain_text_input",
-      params: {
-        block_id: "request_detail",
-        optional: IS_DEBUGGING,
-        multiline: true,
-        label: "依頼内容",
-      },
-    },
-
-    {
-      blockType: "number_input",
-      params: {
-        block_id: "user_id",
-        label: "ユーザーID",
-        optional: IS_DEBUGGING,
-        is_decimal_allowed: true,
-        min_value: "0",
-      },
-    },
-
-    {
-      blockType: "number_input",
-      params: {
-        block_id: "order_id",
-        label: "オーダーID",
-        optional: true,
-        is_decimal_allowed: true,
-        min_value: "0",
-      },
-    },
-
-    {
-      blockType: "static_select",
-      params: {
-        block_id: "severity_type",
-        label: "緊急度",
-        optional: IS_DEBUGGING,
-        options: [
-          {
-            label: "当日必須",
-            value: "in_today",
-          },
-          {
-            label: "3日以内",
-            value: "in_three_days",
-          },
-          {
-            label: "7日以内",
-            value: "in_seven_days",
-          },
-          {
-            label: "1週間以上",
-            value: "over_one_week",
-          },
-        ],
-      },
-    },
-
-    {
-      blockType: "datepicker",
-      params: {
-        label: "対応期限",
-        block_id: "limit_date",
-        optional: IS_DEBUGGING,
-      },
-    },
-  ]
-  
-  );
-};
